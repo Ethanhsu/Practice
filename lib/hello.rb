b9b4024f37b023a8cc9ce589e189ae29e3d11289
Ethan Hsu <Ethan681204@gmail.com>
2011-06-30 15:24:22 +0800

    modify hello.rb after add greeter.rb under branch greet

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,3 +5,5 @@ The first modification of hello.rb
 The second modification of hello.rb before commit since the first modification.
 
 phone number = 0987-654321
+
+The scecond chnage after branch to greet

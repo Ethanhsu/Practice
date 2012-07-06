7ac53eb5427f03792bc3a9e23164adfb4b18710e
Ethanhsu <Ethan681204@gmail.com>
2012-07-06 16:50:27 +0800

    Update master

--- a.rb
+++ a.rb
@@ -1,6 +1,1 @@
-The first line of a.rb
-<<<<<<< HEAD
-I don't know someone modify the a.rb before me . what should i do.s
-=======
-I' CHANGE IT AND COMMIT TO GITHUB BEFORE NB.
->>>>>>> 729cdbc8a29286fc83ab0acb9a94d47908fa94bd
+Clean this shit and start over.

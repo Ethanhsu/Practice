1fcabc2e871a0b563998b5367e7d4ca84449448a
Ethanhsu <Ethan681204@gmail.com>
2012-07-09 13:30:55 +0800

    ask whom

--- a.rb
+++ a.rb
@@ -1,6 +1,6 @@
 # ADD A HEADER COMMENT
 Clean this shit and start over.
-Would you please shut your GG off man?
+Would you please shut your GG off man?John or 
 Hello, hell.PCA,LPGA
 
 Also a real content.

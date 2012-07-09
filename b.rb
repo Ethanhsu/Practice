bcbdbc5560bb060feb742e96f09db470a9bb4105
Ethan Hsu <Ethan681204@gmail.com>
2012-07-09 21:49:41 +0800

    merged and modified

--- b.rb
+++ b.rb
@@ -2,7 +2,7 @@ The first/second line of b.rb
 
 I said,what if god is one of us?
 
-tell me ,who cares?Jesus?Cefiro?Sapporo
+tell me ,who cares?Jesus?Cefiro?Sapporo?Nakoya
 
 I care.everybody does.and you
 
--- c.rb
+++ c.rb
@@ -2,3 +2,4 @@ The first line of c.rb
 1567890
 mamammmamamamama,papapapapapaappa,grandpa
 this is what we want to check into server.
+paparazzi

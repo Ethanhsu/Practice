3307e957dc22ccdcb35302159892056c875a002b
Ethanhsu <Ethan681204@gmail.com>
2012-07-09 13:59:13 +0800

    Jordan involved

--- a.rb
+++ a.rb
@@ -1,6 +1,6 @@
 # ADD A HEADER COMMENT
 Clean this shit and start over.
-Would you please shut your GG off man?Jake or Jackson!Julian  
+Would you please shut your GG off man?Jake or Jackson!Julian ,Jordan 
 Hello, hell.PCA,LPGA
 
 Also a real content.

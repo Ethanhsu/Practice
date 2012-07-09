d60d2e41965b8c77928221799823b0bd63511e0b
Ethan Hsu <Ethan681204@gmail.com>
2012-07-09 13:31:49 +0800

    i thought there is only Jackson

--- a.rb
+++ a.rb
@@ -1,6 +1,6 @@
 # ADD A HEADER COMMENT
 Clean this shit and start over.
-Would you please shut your GG off man?
+Would you please shut your GG off man?Jackson!
 Hello, hell.PCA,LPGA
 
 Also a real content.

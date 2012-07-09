af1d183d36d9e2e28defb9865fbec35a3080c5bd
Ethan Hsu <Ethan681204@gmail.com>
2012-07-09 12:45:03 +0800

    again

--- b.rb
+++ b.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 The first/second line of b.rb
 
 I said,what if god is one of us?
-=======
-The second line of b.rb
-one more question?
-what if god is one of us?z
->>>>>>> origin/master

6892e96f75663b133ad44ef73e8157d5d1356883
Ethan Hsu <Ethan681204@gmail.com>
2012-07-09 13:37:43 +0800

    NOt john , it's jake

--- a.rb
+++ a.rb
@@ -1,6 +1,6 @@
 # ADD A HEADER COMMENT
 Clean this shit and start over.
-Would you please shut your GG off man?John or Jackson!
+Would you please shut your GG off man?Jake or Jackson!
 Hello, hell.PCA,LPGA
 
 Also a real content.

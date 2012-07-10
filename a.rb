449cdbb4bf506277a9fb00ecb2919c7ff6f4a902
Ethanhsu <Ethan681204@gmail.com>
2012-07-10 10:29:31 +0800

    connecting people

--- a.rb
+++ a.rb
@@ -1,4 +1,4 @@
-# ADD A HEADER COMMENT,in PC,testing branch
+# ADD A HEADER COMMENT,in PC,testing branch, nokia
 Clean this shit and start over.
 Would you please shut your GG off man?Jake or Jackson!Julian ,Jordan 
 Hello, hell.PCA,LPGA,NBA,MLB
